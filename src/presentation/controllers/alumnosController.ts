efca2dbad0a46799475be587f5ae1eaaea8d319f
fgalegria <francisco.gomez@comsys.mx>
2024-07-10 11:28:19 -0700

    Se agregaron campos a postalumno

--- src/presentation/controllers/alumnosController.ts
+++ src/presentation/controllers/alumnosController.ts
@@ -30,20 +30,24 @@ export class AlumnosController {
             ap_paterno,
             ap_materno,
             fec_nacimiento,
+            genero,
+            email,
             calle,
             colonia,
             cp,
             municipio,
             estado,
             telefono,
-            carrera
+            carrera,
+            num_exterior,
+            num_interior,
         } = req.body;
 
         try {
             await this.pool.query(
-                `INSERT INTO alumnos (id, nombre, ap_paterno, ap_materno, fec_nacimiento, calle, colonia, cp, municipio, estado, telefono, carrera)
-                VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)`,
-                [uuidv4(), nombre, ap_paterno, ap_materno, fec_nacimiento, calle, colonia, cp, municipio, estado, telefono, carrera]
+                `INSERT INTO alumnos (id, nombre, ap_paterno, ap_materno, fec_nacimiento, genero, email, num_exterior, num_interior, calle, colonia, cp, municipio, estado, telefono, carrera)
+                VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16)`,
+                [uuidv4(), nombre, ap_paterno, ap_materno, fec_nacimiento, genero, email, num_exterior, num_interior, calle, colonia, cp, municipio, estado, telefono, carrera]
             );
 
             return res.status(200).json({ message: 'Alumno creado correctamente' });
